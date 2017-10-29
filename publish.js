cd7a62ee09156e07acf20bd16f4cf64838a5c619
iwate <iwatesoftware@live.jp>
2017-10-29 21:28:44 +0900

    initial commit

---
+++ publish.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const theme = fs.readFileSync(`./src/theme.html`);
+const elements = fs.readdirSync('./src/elements')
+                   .map(file => fs.readFileSync(`./src/elements/${file}`))
+                   .join('');
+
+if (!fs.existsSync('./dist'))
+  fs.mkdirSync('./dist');
+
+fs.writeFileSync('./dist/elements.html', elements)
+fs.writeFileSync('./dist/theme.html', theme)
