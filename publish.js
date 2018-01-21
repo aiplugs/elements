23586a934abcfc87c02958de0fc8d8bf7abc11e1
iwate <iwatesoftware@live.jp>
2018-01-22 01:35:03 +0900

    add monaco

--- publish.js
+++ publish.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
-
+const ignores = ['monaco-iframe.html']
 const theme = fs.readFileSync(`./src/theme.html`);
+const monacoIframe = fs.readFileSync(`./src/elements/monaco-iframe.html`);
 const elements = fs.readdirSync('./src/elements')
+                   .filter(file => ignores.indexOf(file) == -1)
                    .map(file => fs.readFileSync(`./src/elements/${file}`))
                    .join('');
 
@@ -10,3 +12,4 @@ if (!fs.existsSync('./dist'))
 
 fs.writeFileSync('./dist/elements.html', elements)
 fs.writeFileSync('./dist/theme.html', theme)
+fs.writeFileSync('./dist/monaco-iframe.html', monacoIframe)
